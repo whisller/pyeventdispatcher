6dffb7e1ed7b406036f015b07de358b02d44f186
Daniel Ancuta <whisller@gmail.com>
2018-11-25 00:43:36 +0000

    Add “priority”.
    - Add “priority” to registered listener
    - Add “black” as code formatter

--- pyeventdispatcher/__init__.py
+++ pyeventdispatcher/__init__.py
@@ -1,6 +1,10 @@
 from collections import defaultdict
 
 
+class PyEventDispatcherException(Exception):
+    pass
+
+
 class PyEvent:
     def __init__(self, name, data):
         self.name = name
@@ -11,10 +15,20 @@ class PyEventDispatcher:
     def __init__(self):
         self._listeners = defaultdict(list)
 
-    def register(self, event_name, listener):
-        self._listeners[event_name].append(listener)
+    def register(self, event_name, listener, position=0):
+        if not callable(listener):
+            raise PyEventDispatcherException(f'"{listener}" is not callable.')
+
+        try:
+            float(position)
+        except (ValueError, TypeError):
+            raise PyEventDispatcherException(f'"{position}" is not numeric.')
+
+        self._listeners[event_name].append({"listener": listener, "position": position})
 
     def dispatch(self, event):
         if event.name in self._listeners:
-            for listener in self._listeners[event.name]:
-                listener(event)
+            for info in sorted(
+                self._listeners[event.name], key=lambda x: x["position"]
+            ):
+                info["listener"](event)
--- test/test_pyeventdispatcher.py
+++ test/test_pyeventdispatcher.py
@@ -1,21 +1,71 @@
-from pyeventdispatcher import PyEventDispatcher, PyEvent
+import pytest
+
+from pyeventdispatcher import PyEventDispatcher, PyEvent, PyEventDispatcherException
 
 
-class TestExecutionOrder:
-    def test_listeners_executed_in_order_they_were_added(self, capsys):
-        def first_function(event):
-            print("First")
+class TestRegister:
+    class MyListener:
+        def call_on_event(self, event):
             print(event.data)
 
-        def second_function(event):
-            print("Second")
-            print(event.data)
-
+    @pytest.mark.parametrize(
+        "registered", [MyListener().call_on_event, lambda event: print(event.data)]
+    )
+    def test_it_allows_to_register(self, registered, capsys):
         py_event_dispatcher = PyEventDispatcher()
-        py_event_dispatcher.register("foo.bar", first_function)
-        py_event_dispatcher.register("foo.bar", second_function)
+        py_event_dispatcher.register("foo.bar", registered)
         py_event_dispatcher.dispatch(PyEvent("foo.bar", {"a": "b"}))
 
         captured = capsys.readouterr()
 
-        assert captured.out == "First\n{'a': 'b'}\nSecond\n{'a': 'b'}\n"
+        assert captured.out == "{'a': 'b'}\n"
+
+    @pytest.mark.parametrize(
+        "to_register, output",
+        [
+            # With default "priority" - in order they were added
+            (
+                (
+                    {"lambda": lambda event: print("First"), "priority": 0},
+                    {"lambda": lambda event: print("Second"), "priority": 0},
+                ),
+                "First\nSecond\n",
+            ),
+            # Based on priority
+            (
+                (
+                    {"lambda": lambda event: print("First"), "priority": 0},
+                    {"lambda": lambda event: print("Second"), "priority": -100},
+                ),
+                "Second\nFirst\n",
+            ),
+        ],
+    )
+    def test_listeners_executed_in_order(self, to_register, output, capsys):
+        py_event_dispatcher = PyEventDispatcher()
+        for register in to_register:
+            py_event_dispatcher.register(
+                "foo.bar", register["lambda"], register["priority"]
+            )
+        py_event_dispatcher.dispatch(PyEvent("foo.bar", {"a": "b"}))
+
+        captured = capsys.readouterr()
+
+        assert captured.out == output
+
+    def test_it_raises_an_exception_when_non_callable_is_trying_to_be_registered(self):
+        py_event_dispatcher = PyEventDispatcher()
+        with pytest.raises(PyEventDispatcherException):
+            py_event_dispatcher.register("foo.bar", "")
+
+    @pytest.mark.parametrize(
+        "priority",
+        [
+            None,
+            ""
+        ],
+    )
+    def test_it_raises_an_exception_when_priority_is_not_integer(self, priority):
+        py_event_dispatcher = PyEventDispatcher()
+        with pytest.raises(PyEventDispatcherException):
+            py_event_dispatcher.register("foo.bar", lambda event: print(event), priority)
