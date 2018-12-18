07c9431f7d79cff68e3cb7976e6f68bf98bbcd9d
Daniel Ancuta <whisller@gmail.com>
2018-12-18 18:55:56 +0000

    Set version to “0.2.0a0”.

--- pyeventdispatcher/__init__.py
+++ pyeventdispatcher/__init__.py
@@ -9,7 +9,7 @@ from pyeventdispatcher.event_dispatcher import (
     register_event_subscribers,
 )
 
-__version__ = "0.1.0a0"
+__version__ = "0.2.0a0"
 
 register = register_global_listener
 dispatch = dispatch_global_event
